2dc98b100c1595edfe1feed8fd39549a4da0692b
rkunc13 <rkunc13@freeuni.edu.ge>
2016-06-04 11:51:40 +0400

    created empty classes for Nutsa to fill

--- FinalProject/src/Database/DBInfo.java
+++ FinalProject/src/Database/DBInfo.java
@@ -1,5 +1,8 @@
 package Database;
 
 public class DBInfo {
-	private int p = 2;
+	public static final String MYSQL_USERNAME = "a06598_quizzme";
+	public static final String MYSQL_PASSWORD = "webquizz1";
+	public static final String MYSQL_DATABASE_SERVER = "MYSQL5013.Smarterasp.net";
+	public static final String MYSQL_DATABASE_NAME = "db_a06598_quizzme";
 }
