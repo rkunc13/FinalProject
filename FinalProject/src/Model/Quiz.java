2ebfc63008d8999ed925fcac44b24cecb61ffd23
Nuca <nnazg13@freeuni.edu.ge>
2016-06-05 14:29:57 +0700

    Implement Quiz

--- FinalProject/src/Model/Quiz.java
+++ FinalProject/src/Model/Quiz.java
@@ -6,8 +6,18 @@ public class Quiz {
 	
 	private int quizID;
 	private String name;
+	private String description;
 	private int authorID;
+	private boolean randomOrder;
+	private boolean multiplePages;
+	private boolean immediateCorrection;
 	private Timestamp dateCreated;
+	private int points;
+	private boolean reported;
+	private String category;
+	private String tags;
+	private int score;
+	private int quizTakerID;
 
 	
 	public Quiz(int quizID, String name, Timestamp dt, int authorID) {
@@ -15,6 +25,16 @@ public class Quiz {
 		this.name = name;
 		this.dateCreated = dt;
 		this.authorID = authorID;
+	}
+	
+	//another extended constructors 1
+	public Quiz(int quizID, String name, String description, int authorID, boolean randO, boolean multP, boolean immC, Timestamp dt, int points, boolean reported, String category, String tags) {
+		
+	}
+	
+	//another less extended constructors 2
+	public Quiz(int quizID, String name, Timestamp dt, int score, int quizTakerID) {
+		
 	}
 	
 }
