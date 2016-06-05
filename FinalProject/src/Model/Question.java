e9dd6aa6610c1c3cbd3075691fb5c688b92eea0b
Nuca <nnazg13@freeuni.edu.ge>
2016-06-05 16:19:35 +0700

    Question

--- FinalProject/src/Model/Question.java
+++ FinalProject/src/Model/Question.java
@@ -2,4 +2,18 @@ package Model;
 
 public abstract class Question implements java.io.Serializable {
 	
+	abstract String getQuestionText();
+	abstract int getQuizID();
+	
+	private static final long serialVersionUID = 1L;
+	protected int quizID;
+	protected int authorID;
+	protected String question;
+	protected String answer;
+	
+	abstract long getSerialVersionUID();
+	abstract int getAuthorID();
+	abstract String getQuestion();
+	abstract String setAnswer();
+	
 }
