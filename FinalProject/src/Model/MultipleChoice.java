cfccaf4b37c617f2b13c5d5ae36a3f0738409f11
Nuca <nnazg13@freeuni.edu.ge>
2016-06-05 18:14:03 +0700

    Implement Multiple Choice

--- FinalProject/src/Model/MultipleChoice.java
+++ FinalProject/src/Model/MultipleChoice.java
@@ -1,5 +1,29 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MultipleChoice {
 
+
+	public MultipleChoice() {
+	}
+
+	public int getQuizID() {
+	}
+
+	public int getNumChoices() {
+	}
+
+	public String getQuestionText() {
+	}
+
+	public String getChoicesAsText() {
+	}
+
+	public ArrayList<String> getChoicesAsList() {
+	}
+
+	public String getAnswerText() {
+	}
 }
