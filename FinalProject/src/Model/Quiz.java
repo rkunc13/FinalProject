c495657179dcf8140b421c9c19fcc199b13499bb
Nuca <nnazg13@freeuni.edu.ge>
2016-06-05 14:08:48 +0700

    Start to implement Quiz

--- FinalProject/src/Model/Quiz.java
+++ FinalProject/src/Model/Quiz.java
@@ -1,5 +1,21 @@
 package Model;
 
-public class Quiz {
+import java.sql.Timestamp;
 
+public class Quiz {
+	
+	private int quizID;
+	private String name;
+	private int authorID;
+	private Timestamp dateCreated;
+
+	
+	public Quiz(int quizID, String name, Timestamp dt, int authorID) {
+		this.quizID = quizID;
+		this.name = name;
+		this.dateCreated = dt;
+		this.authorID = authorID;
+	}
+	
 }
+	
