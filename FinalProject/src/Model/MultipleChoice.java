3f2e01d2bf984c5eea5cf35230fc846bc00814c3
Nuca <nnazg13@freeuni.edu.ge>
2016-06-06 01:23:03 +0700

    Implement Initializer for Multiple Choice

--- FinalProject/src/Model/MultipleChoice.java
+++ FinalProject/src/Model/MultipleChoice.java
@@ -3,27 +3,18 @@ package Model;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MultipleChoice {
+public class MultipleChoice extends Question {
 
+	private static final long serialVersionUID = 1L;
+	private String choices;
 
-	public MultipleChoice() {
+	public MultipleChoice(int quizID, int authorID, String prompt,
+			String choices, String answer) {
+		this.quizID = quizID;
+		this.authorID = authorID;
+		this.question = prompt;
+		this.choices = choices;
+		this.answer = answer;
 	}
 
-	public int getQuizID() {
-	}
-
-	public int getNumChoices() {
-	}
-
-	public String getQuestionText() {
-	}
-
-	public String getChoicesAsText() {
-	}
-
-	public ArrayList<String> getChoicesAsList() {
-	}
-
-	public String getAnswerText() {
-	}
 }
