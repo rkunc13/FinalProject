5a334ed5f4d36fa6376d2374b27f8dbfbe18852f
Nuca <nnazg13@freeuni.edu.ge>
2016-06-05 17:18:54 +0700

    Constructor QR

--- FinalProject/src/Model/QuestionResponse.java
+++ FinalProject/src/Model/QuestionResponse.java
@@ -4,6 +4,9 @@ public class QuestionResponse extends Question {
 
 
 	public QuestionResponse(int quizID, int authorID, String prompt, String answer) {
-		
+		this.quizID = quizID;
+		this.authorID = authorID;
+		this.question = prompt;
+		this.answer = answer;
 	}
 }
