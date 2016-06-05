bfa435ab884479853403d44f5ef2dee0a78c7636
Nuca <nnazg13@freeuni.edu.ge>
2016-06-05 23:06:46 +0700

    Picture Response Constructor

--- FinalProject/src/Model/PictureResponse.java
+++ FinalProject/src/Model/PictureResponse.java
@@ -1,15 +1,13 @@
 package Model;
 
-public class PictureResponse {
+public class PictureResponse extends Question {
+
+	private static final long serialVersionUID = 1L;
 
 	public PictureResponse(int quizID, int authorID, String url, String answer) {
-	}
-
-	public int getQuizID() {
-	}
-
-	public String getQuestionText() {}
-
-	public String getAnswerText() {
+		this.quizID = quizID;
+		this.authorID = authorID;
+		this.question = url;
+		this.answer = answer;
 	}
 }
