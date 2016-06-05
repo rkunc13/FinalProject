a9afe4abc4faeb539f641b8ab759c33f048569d9
Nuca <nnazg13@freeuni.edu.ge>
2016-06-05 17:43:03 +0700

    Question Response

--- FinalProject/src/Model/QuestionResponse.java
+++ FinalProject/src/Model/QuestionResponse.java
@@ -2,11 +2,25 @@ package Model;
 
 public class QuestionResponse extends Question {
 
+	private static final long serialVersionUID = 1L;
 
-	public QuestionResponse(int quizID, int authorID, String prompt, String answer) {
+	public QuestionResponse(int quizID, int authorID, String prompt,
+			String answer) {
 		this.quizID = quizID;
 		this.authorID = authorID;
 		this.question = prompt;
 		this.answer = answer;
+	}
+
+	public int getQuizID() {
+		return quizID;
+	}
+
+	public String getQuestionText() {
+		return question;
+	}
+
+	public String getAnswerText() {
+		return answer.toLowerCase();
 	}
 }
