d1658432a07c4fd092ae2e56f28756c9e12b4439
Nuca <nnazg13@freeuni.edu.ge>
2016-06-05 16:46:42 +0700

    Implement QR

--- FinalProject/src/Model/QuestionResponse.java
+++ FinalProject/src/Model/QuestionResponse.java
@@ -1,5 +1,18 @@
 package Model;
 
-public class QuestionResponse {
+public class QuestionResponse extends Question {
 
+	private static final long serialVersionUID = 1L;
+
+	public QuestionResponse(int quizID, String prompt, String answer) {
+	}
+
+	public int getQuizID() {
+	}
+
+	public String getQuestionText() {
+	}
+
+	public String getAnswerText() {
+	}
 }
