2496173d6b070bbb61bc89fe98bd031ab8bfddea
Nuca <nnazg13@freeuni.edu.ge>
2016-06-06 11:27:06 +0700

    Implement filling in blanks

--- FinalProject/src/Model/FillInTheBlank.java
+++ FinalProject/src/Model/FillInTheBlank.java
@@ -2,4 +2,25 @@ package Model;
 
 public class FillInTheBlank {
 
+
+	public FillInTheBlank(int quizID, int authorID, String prompt, String answer) {
+		
+	}
+
+	public int getQuizID() {
+	}
+
+	public int getNumBlanks() {
+	}
+	public String getQuestionText() {
+	}
+
+	public String getQuestionRegex() {
+	}
+
+	public String getAnswerAsText() {
+	}
+	public ArrayList<String> getAnswerAsList() {
+	}
+
 }
