34ce2c795b11f811e5bc16780711f628f958546c
Nuca <nnazg13@freeuni.edu.ge>
2016-06-07 11:13:40 +0700

    Start challenge

--- FinalProject/src/Model/Challenge.java
+++ FinalProject/src/Model/Challenge.java
@@ -1,5 +1,23 @@
 package Model;
 
 public class Challenge {
+	int senderID;
+	int receiverID;
+	int quizID;
+	int score;
 
+	public Challenge() {
+	}
+
+	public int getSenderId() {
+	}
+
+	public int getReceiverId() {
+	}
+
+	public int getQuizId() {
+	}
+
+	public int getScore() {
+	}
 }
