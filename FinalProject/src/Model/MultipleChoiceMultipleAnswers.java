e07c102beba77127d9509c5153532ca80e5e1de4
Nuca <nnazg13@freeuni.edu.ge>
2016-06-09 01:39:18 +0700

    Questions with many possible answers

--- FinalProject/src/Model/MultipleChoiceMultipleAnswers.java
+++ FinalProject/src/Model/MultipleChoiceMultipleAnswers.java
@@ -1,9 +1,35 @@
 package Model;
 
-public class MultipleChoiceMultipleAnswers {
 
-	public MultipleChoiceMultipleAnswers() {
+public class MultipleChoiceMultipleAnswers extends Question {
+
+	private static final long serialVersionUID = 1L;
+	private String choices;
+
+	public MultipleChoiceMultipleAnswers(int quizID, int authorID, String prompt, String choices, String answer) {
 	}
 
-	
+	public int getQuizID() {
+	}
+
+	public int getNumChoices() {
+	}
+
+	public int getNumAnswers() {
+	}
+
+	public String getQuestionText() {
+	}
+
+	public String getChoicesAsText() {
+	}
+
+	public String getAnswerAsText() {
+	}
+
+	public ArrayList<String> getChoicesAsList() {
+	}
+
+	public ArrayList<String> getAnswerAsList() {
+	}
 }
