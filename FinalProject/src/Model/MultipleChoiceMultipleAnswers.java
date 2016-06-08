b0937b83bd952c4d067c26afc364e61fee578e28
Nuca <nnazg13@freeuni.edu.ge>
2016-06-09 04:06:49 +0700

    Multiple Choices

--- FinalProject/src/Model/MultipleChoiceMultipleAnswers.java
+++ FinalProject/src/Model/MultipleChoiceMultipleAnswers.java
@@ -3,33 +3,11 @@ package Model;
 
 public class MultipleChoiceMultipleAnswers extends Question {
 
-	private static final long serialVersionUID = 1L;
-	private String choices;
-
-	public MultipleChoiceMultipleAnswers(int quizID, int authorID, String prompt, String choices, String answer) {
+	public MultipleChoiceMultipleAnswers(int quizID, int authorID, String prompt, String answer) {
+		this.quizID = quizID;
+		this.authorID = authorID;
+		this.question = prompt;
+		this.answer = answer;
 	}
 
-	public int getQuizID() {
-	}
-
-	public int getNumChoices() {
-	}
-
-	public int getNumAnswers() {
-	}
-
-	public String getQuestionText() {
-	}
-
-	public String getChoicesAsText() {
-	}
-
-	public String getAnswerAsText() {
-	}
-
-	public ArrayList<String> getChoicesAsList() {
-	}
-
-	public ArrayList<String> getAnswerAsList() {
-	}
 }
