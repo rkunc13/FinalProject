3574d78139fecb21e7c7f698e14d369577468e1e
Nuca <nnazg13@freeuni.edu.ge>
2016-06-10 03:16:04 +0700

    Keep doing right

--- FinalProject/src/Model/User.java
+++ FinalProject/src/Model/User.java
@@ -2,23 +2,9 @@ package Model;
 
 public class User {
 
-	private int id;
-	private String email;
-	private String name;
-	private boolean admin;
 
 	public User(int id, String email, String name, boolean admin) {
+		
 	}
 
-	public int getId() {
-	}
-
-	public String getEmail() {
-	}
-
-	public String getName() {
-	}
-
-	public boolean getAdmin() {
-	}
 }
