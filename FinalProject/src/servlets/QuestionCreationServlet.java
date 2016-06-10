ef33d657de1ac2e61b015ecdaa6b34ba8392afdf
Nuca <nnazg13@freeuni.edu.ge>
2016-06-10 18:39:11 +0700

    Implement Servlets

--- FinalProject/src/servlets/QuestionCreationServlet.java
+++ FinalProject/src/servlets/QuestionCreationServlet.java
@@ -1,19 +1,7 @@
 package servlets;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import Database.UserData;
-
-@WebServlet("/QuestionCreationServlet")
 public class QuestionCreationServlet extends HttpServlet {
 	
 }
