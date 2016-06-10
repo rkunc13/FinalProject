afda15f05822d60b31af9f023f879304f2e2fb11
Nuca <nnazg13@freeuni.edu.ge>
2016-06-10 11:45:03 +0700

    Extend Multiple Choices

--- FinalProject/src/Model/MultipleChoiceMultipleAnswers.java
+++ FinalProject/src/Model/MultipleChoiceMultipleAnswers.java
@@ -1,13 +1,73 @@
 package Model;
 
+import java.util.ArrayList;
+
+import java.util.Arrays;
 
 public class MultipleChoiceMultipleAnswers extends Question {
 
-	public MultipleChoiceMultipleAnswers(int quizID, int authorID, String prompt, String answer) {
+	private static final long serialVersionUID = 1L;
+	private String choices;
+
+	public MultipleChoiceMultipleAnswers(int quizID, int authorID,
+			String prompt, String choices, String answer) {
+		
 		this.quizID = quizID;
 		this.authorID = authorID;
 		this.question = prompt;
+		this.choices = choices;
 		this.answer = answer;
+		
 	}
 
+	public int getQuizID() {
+		
+		return quizID;
+		
+	}
+
+	public int getNumChoices() {
+		
+		return choices.split(",").length;
+		
+	}
+
+	public int getNumAnswers() {
+		
+		return getAnswerAsList().size();
+		
+	}
+
+	public String getQuestionText() {
+		
+		return question;
+		
+	}
+
+	public String getChoicesAsText() {
+		
+		return choices;
+		
+	}
+
+	public String getAnswerAsText() {
+		
+		return answer;
+		
+	}
+
+	public ArrayList<String> getChoicesAsList() {
+		
+		return new ArrayList<String>(Arrays.asList(choices.trim().toLowerCase()
+				.split("\\s*,\\s*")));
+		
+	}
+
+	public ArrayList<String> getAnswerAsList() {
+		
+		return new ArrayList<String>(Arrays.asList(answer.trim().toLowerCase()
+				.split("\\s*,\\s*")));
+		
+	}
+	
 }
