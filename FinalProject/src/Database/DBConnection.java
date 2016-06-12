b1da075880c87fe59bf4f11ac47657b72f0b0971
Nuca <nnazg13@freeuni.edu.ge>
2016-06-12 12:26:27 +0700

    Implement DB Connection

--- FinalProject/src/Database/DBConnection.java
+++ FinalProject/src/Database/DBConnection.java
@@ -1,5 +1,32 @@
 package Database;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBConnection {
+
+	private Connection con;
+	private Statement stmt;
+
+	public DBConnection() {
+		
+	}
+
+
+	public Connection getConnection() {
+		return con;
+	}
+	
+	
+	public void close() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
