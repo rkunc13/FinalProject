4f891b5abdf35cf8542719a1127573a25c3db89f
Nuca <nnazg13@freeuni.edu.ge>
2016-06-13 22:28:01 +0700

    back to DB Connection

--- FinalProject/src/Database/DBConnection.java
+++ FinalProject/src/Database/DBConnection.java
@@ -8,19 +8,39 @@ import java.sql.Statement;
 
 public class DBConnection {
 
+	static String account = DBInfo.MYSQL_USERNAME;
+	static String password = DBInfo.MYSQL_PASSWORD;
+	static String server = DBInfo.MYSQL_DATABASE_SERVER;
+	static String database = DBInfo.MYSQL_DATABASE_NAME;
 	private Connection con;
 	private Statement stmt;
 
 	public DBConnection() {
-		
+		try {
+			// open the connection to the database
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://" + server, account, password);
+			
+			// create a new Statement object
+			stmt = con.createStatement();
+			stmt.executeQuery("USE " + database);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
-
-
+	
+	/**
+	 * Returns the mySQL database Connection Object
+	 */
 	public Connection getConnection() {
 		return con;
 	}
 	
-	
+	/**
+	 * Close the connection to the database on exit
+	 */
 	public void close() {
 		try {
 			con.close();
