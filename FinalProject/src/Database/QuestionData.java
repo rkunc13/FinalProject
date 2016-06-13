7b7222f3e6874bcc63aa650b51be2da5cb1e8ca1
Nuca <nnazg13@freeuni.edu.ge>
2016-06-13 23:59:02 +0700

    Implement data for question

--- FinalProject/src/Database/QuestionData.java
+++ FinalProject/src/Database/QuestionData.java
@@ -1,5 +1,33 @@
 package Database;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuestionData {
 
+	public QuestionData() {
+	}
+	
+	public void addQuestion() {
+		
+	}
+	
+	public void updateQuestion() {
+		
+	}
+	
+	public List<Integer> getQuestionIDs(int quiz_id) {
+		List<Integer> q = new ArrayList<Integer>();
+		
+		return q;
+	}
+	
+	
+	public Object getQuestionByID(int id) {
+		return null;
+	}
+	
+	public String getTypeByID() {
+		return "nu awitlebs vax";
+	}
 }
