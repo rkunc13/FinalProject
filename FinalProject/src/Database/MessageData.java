0ab090ec1e2c07bdc10cee07fb1c90a962ff55a9
Nuca <nnazg13@freeuni.edu.ge>
2016-06-18 02:56:05 +0700

    Data for Messages

--- FinalProject/src/Database/MessageData.java
+++ FinalProject/src/Database/MessageData.java
@@ -1,5 +1,45 @@
 package Database;
 
-public class MessageData {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import Model.Challenge;
+
+public class MessageData {
+	private Connection con;
+	private ResultSet rs;
+
+	public MessageData(Connection con) {
+		this.con = con;
+	}
+
+	public void sendMessage(String message, String senderEmail,
+			String receiverEmail) {
+		
+	}
+	
+	public List<String> getMessage() {
+		List<String> m = new ArrayList<String>();
+		return m;
+	}
+	
+	public void sendChallenge() {
+		
+	}
+
+	public List<Challenge> getChallenge(int receiverID) {
+		List<Challenge> challengeList = new ArrayList<Challenge>();
+		return challengeList;
+	}
+
+	public void deleteChallenge() {
+		
+	}
+
+	public boolean alreadyChallenged() {
+		return true;
+	}
 }
