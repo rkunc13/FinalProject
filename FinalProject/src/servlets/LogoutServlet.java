8c0fcaede3e570240a58ba93b7d8329a9b3a8d6e
rati <rkunc13@freeuni.edu.ge>
2016-06-21 01:33:07 +0700

    remake logout servlet

--- FinalProject/src/servlets/LogoutServlet.java
+++ FinalProject/src/servlets/LogoutServlet.java
@@ -1,7 +1,44 @@
 package servlets;
 
-import javax.servlet.http.HttpServlet;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
-	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.removeAttribute("user id");
+		session.removeAttribute("email");
+		RequestDispatcher disp = request.getRequestDispatcher("index.jsp");
+		disp.forward(request, response);
+	}
+
 }
