8e7f31d1c792c8deae1bbde5f511bc5a88237428
rati <rkunc13@freeuni.edu.ge>
2016-06-21 05:45:39 +0700

    remake removing quiz servlet

--- FinalProject/src/servlets/AdminRemoveQuizServlet.java
+++ FinalProject/src/servlets/AdminRemoveQuizServlet.java
@@ -1,7 +1,51 @@
 package servlets;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Database.AdminData;
+
+@WebServlet("/AdminRemoveQuizServlet")
 public class AdminRemoveQuizServlet extends HttpServlet {
-	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminRemoveQuizServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		AdminData adManager = ((AdminData)request.getServletContext().getAttribute("admin manager"));
+		adManager.historyClearForQuiz(id);
+		adManager.questionClearForQuiz(id);
+		adManager.removeQuiz(id);
+		if (request.getParameter("admin_edit").equals("true")) {
+			RequestDispatcher disp = request.getRequestDispatcher("admin_homepage.jsp");
+			disp.forward(request, response);
+		} else {
+			RequestDispatcher disp = request.getRequestDispatcher("homepage.jsp");
+			disp.forward(request, response);
+		}
+	}
+
 }
