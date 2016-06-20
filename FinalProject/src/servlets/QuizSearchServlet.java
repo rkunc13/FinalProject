1386959f14909c6f97b34f8221299beeebb801e5
rati <rkunc13@freeuni.edu.ge>
2016-06-20 09:27:48 +0700

    remake quiz search resvlet

--- FinalProject/src/servlets/QuizSearchServlet.java
+++ FinalProject/src/servlets/QuizSearchServlet.java
@@ -1,7 +1,52 @@
 package servlets;
 
-import javax.servlet.http.HttpServlet;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Database.QuizData;
+import Model.Quiz;
+
+@WebServlet("/QuizSearchServlet")
 public class QuizSearchServlet extends HttpServlet {
-	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public QuizSearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		QuizData qManager = (QuizData) getServletContext().getAttribute("quiz manager");
+		String name = request.getParameter("name");
+		String category = request.getParameter("category");
+		String tag = request.getParameter("tag");		
+		ArrayList<Quiz> res = qManager.searchQuizzes(name, category, tag);
+		session.setAttribute("search", res);		
+		RequestDispatcher disp = request.getRequestDispatcher("quiz_archive.jsp");
+		disp.forward(request, response);
+	}
+
 }
