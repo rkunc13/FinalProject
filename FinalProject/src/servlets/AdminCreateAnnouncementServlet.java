70f3450c2ceb324e74f8a408a1db50c274a3f945
rati <rkunc13@freeuni.edu.ge>
2016-06-21 00:30:54 +0700

    remake admin create announcement servlet

--- FinalProject/src/servlets/AdminCreateAnnouncementServlet.java
+++ FinalProject/src/servlets/AdminCreateAnnouncementServlet.java
@@ -1,7 +1,44 @@
 package servlets;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Database.AdminData;
+
+@WebServlet("/AdminCreateAnnouncementServlet")
 public class AdminCreateAnnouncementServlet extends HttpServlet {
-	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminCreateAnnouncementServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String announcement = request.getParameter("announcement");
+		AdminData adminData = ((AdminData)request.getServletContext().getAttribute("admin manager"));
+		adminData.addAnnouncement(announcement);
+		RequestDispatcher disp = request.getRequestDispatcher("admin_homepage.jsp");
+		disp.forward(request, response);
+	}
+
 }
