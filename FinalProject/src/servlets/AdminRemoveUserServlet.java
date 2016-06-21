627b1dc2301f47cf6d8aa62dcb2dae35b3de5016
rati <rkunc13@freeuni.edu.ge>
2016-06-21 11:46:26 +0700

    now it's enough for admin servlets

--- FinalProject/src/servlets/AdminRemoveUserServlet.java
+++ FinalProject/src/servlets/AdminRemoveUserServlet.java
@@ -1,7 +1,45 @@
 package servlets;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Database.AdminData;
+
+@WebServlet("/AdminRemoveUserServlet")
 public class AdminRemoveUserServlet extends HttpServlet {
-	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminRemoveUserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		AdminData adManager = ((AdminData)request.getServletContext().getAttribute("admin manager"));
+		adManager.removeUserAccount(id);
+		RequestDispatcher disp = request.getRequestDispatcher("admin_homepage.jsp");
+		disp.forward(request, response);
+	}
+
 }
+
