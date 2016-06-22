494c41ce739e816d7ad1d4b5049c2dc2873cf2ff
rati <rkunc13@freeuni.edu.ge>
2016-06-23 01:37:38 +0700

    find friend

--- FinalProject/src/servlets/UserSearchServlet.java
+++ FinalProject/src/servlets/UserSearchServlet.java
@@ -1,7 +1,43 @@
 package servlets;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/UserSearchServlet")
 public class UserSearchServlet extends HttpServlet {
-	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserSearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String info = request.getParameter("info");
+		session.setAttribute("info", info);
+		RequestDispatcher disp = request.getRequestDispatcher("search_user.jsp");
+		disp.forward(request, response);
+	}
+
 }
