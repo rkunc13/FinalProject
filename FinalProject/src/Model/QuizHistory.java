74dc55f16a60022b7c5ff472671bce465e4f2e52
rati <rkunc13@freeuni.edu.ge>
2016-06-28 13:05:45 +0700

    try to add History

--- FinalProject/src/Model/QuizHistory.java
+++ FinalProject/src/Model/QuizHistory.java
@@ -1,5 +1,65 @@
 package Model;
 
+import java.sql.Timestamp;
+
 public class QuizHistory {
 
+	private int quizHistoryId;
+	private int quizId;
+	private int userId;
+	private int score;
+	private int total;
+	private int rating;
+	private String review;
+	private String name;
+	private Timestamp dt;
+
+	public QuizHistory(int quizHistoryID, int quizID, int userID, int score,
+			int total, int rating, String review, String name, Timestamp dt) {
+		this.quizHistoryId = quizHistoryID;
+		this.quizId = quizID;
+		this.userId = userID;
+		this.score = score;
+		this.total = total;
+		this.rating = rating;
+		this.review = review;
+		this.name = name;
+		this.dt = dt;
+	}
+
+	public int getQuizHistoryId() {
+		return quizHistoryId;
+	}
+
+	public int getQuizId() {
+		return quizId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public String getReview() {
+		return review;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Timestamp getTimeStamp() {
+		return dt;
+	}
 }
